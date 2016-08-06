fbb00be7ebe94ab78a9dfa04035c2044baab2048
Noah Ryan Holt <noahryanholt@tutanota.de>
2016-08-06 14:18:08 -0500

    Fixed error in cmus_now_playing.sh that showed duplicate of artist name

--- conky/cmus_now_playing.sh
+++ conky/cmus_now_playing.sh
@@ -4,7 +4,7 @@ echo "cmus is not installed."
 exit
 fi
 
-ARTIST=$( cmus-remote -Q 2>/dev/null | grep artist | cut -d " " -f 3- )
+ARTIST=$( cmus-remote -Q 2>/dev/null | grep -v "albumartist" | grep artist | cut -d " " -f 3- )
 TITLE=$( cmus-remote -Q 2>/dev/null | grep title | cut -d " " -f 3- )
 
 if [ -z "$ARTIST" ];
