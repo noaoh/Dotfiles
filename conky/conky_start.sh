a9e3e8a0a870b016bd99d1f091170fff12061dcb
Noah Ryan Holt <noahryanholt@tutanota.de>
2016-08-24 09:57:37 -0500

    Changed file name for right conky

--- conky/conky_start.sh
+++ conky/conky_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-conky -c ~/.config/conky/conkytodotxt-right.conf
+conky -c ~/.config/conky/conkygcal-right.conf
 conky -c ~/.config/conky/conkygcal-center.conf
 conky -c ~/.config/conky/conkygcal-left.conf
