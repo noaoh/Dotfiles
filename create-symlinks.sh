cfdef0476c854e5dddd9d6590e72892679163a35
Noah Holt <noahryanholt@tutanota.de>
2016-12-20 16:17:48 -0600

    Formatted create-symlinks.sh and added .profile symlinks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-ln -s ~/.vimrc ~/Dotfiles/.vimrc
-ln -s ~/.bashrc ~/Dotfiles/.bashrc
-ln -s ~/.Xresources ~/Dotfiles/xresources/Xresources.light
-ln -s ~/.local/share/fonts ~/Dotfiles/fonts/terminus
-ln -s ~/.local/share/fonts ~/Dotfiles/fonts/small-type-writing
-ln -s ~/.local/share/fonts ~/Dotfiles/fonts/courierprime-code
+ln -s ~/Dotfiles/.vimrc ~/.vimrc 
+
+ln -s ~/Dotfiles/.bashrc ~/.bashrc 
+
+ln -s ~/Dotfiles/xresources/Xresources.light ~/.Xresources 
+
+ln -s ~/Dotfiles/fonts/terminus ~/.local/share/fonts 
+ln -s ~/Dotfiles/fonts/small-type-writing ~/.local/share/fonts 
+ln -s ~/Dotfiles/fonts/courierprime-code ~/.local/share/fonts 
+
+mkdir ~/.config/conky
+ln -s ~/Dotfiles/conky/conky.conf ~/.config/conky/conky.conf 
+ln -s ~/Dotfiles/conky/cmus_now_playing.sh ~/.config/conky/cmus_now_playing.sh
+
+ln -sf ~/Dotfiles/.profile ~/.profile
+ln -sf ~/.profile ~/.bash_profile
+
+mkdir ~/.cmus
+ln -s ~/Dotfiles/red.theme ~/.cmus
