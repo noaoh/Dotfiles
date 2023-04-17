e805e1cc7bac97a0edd0ebb85ece3fad2aae1557
Noah Holt <noahryanholt@gmail.com>
2023-04-16 19:09:14 -0500

    awesomewm new desktop + switch monitors

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -216,7 +216,7 @@ local function client_menu_toggle_fn()
 
                                 -- Each screen has its own tag table.
                                 -- awful.tag({ "www", "code", "music", "office", "misc", "6", "7", "8", "9"}, s, awful.layout.layouts[1])
-                                awful.tag({ "www", "code", "music", "office", "tools", "comm", "tmp" }, s, awful.layout.layouts[1])
+                                awful.tag({ "www", "code", "music", "office", "tools", "demo", "comm", "tmp" }, s, awful.layout.layouts[1])
 
                                 -- Create a promptbox for each screen
                                 s.mypromptbox = awful.widget.prompt()
@@ -281,7 +281,7 @@ local function client_menu_toggle_fn()
         awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
         {description = "go back", group = "tag"}),
         awful.key({ modkey, }, "z", function() quake:toggle() end),
-        awful.key({ modkey, "Control" }, "l", function() awful.util.spawn("i3lock -i " .. lockscreen_wallpaper_path) end),
+        awful.key({ modkey, "Shift" }, "l", function() awful.util.spawn("i3lock -i " .. lockscreen_wallpaper_path) end),
         awful.key({ modkey, }, "j",
         function()
                 awful.client.focus.bydirection("down")
@@ -322,9 +322,9 @@ local function client_menu_toggle_fn()
         {description = "swap with next client by index", group = "client"}),
         awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
         {description = "swap with previous client by index", group = "client"}),
-        awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+        awful.key({ modkey, "Control" }, "h", function () awful.screen.focus_relative( 1) end,
         {description = "focus the next screen", group = "screen"}),
-        awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+        awful.key({ modkey, "Control" }, "l", function () awful.screen.focus_relative(-1) end,
         {description = "focus the previous screen", group = "screen"}),
         awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
         {description = "jump to urgent client", group = "client"}),
